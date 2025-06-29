b6bac13628931fd904eb9efbb579e5e9a00ece2c
TeeloKay <143880606+TeeloKay@users.noreply.github.com>
2025-06-29 19:39:51 +0200

    updated dark theme, scrapped theme switch for now.

--- scripts/controllers/theme_controller.gd
+++ scripts/controllers/theme_controller.gd
@@ -3,11 +3,18 @@ class_name ThemeController extends MenuController
 @export var dark_theme: Theme
 @export var light_theme: Theme
 
+@export var toggle: CheckButton
 @export var root: Control
+
+@export var dark_mode: bool = true:
+	set = set_dark_mode
 
 func _ready() -> void:
 	super._ready()
+	toggle.button_pressed = dark_mode
+	toggle.toggled.connect(set_dark_mode)
+	
+func set_dark_mode(mode: bool) -> void:
+	dark_mode = mode
 	if root:
-		root.theme = light_theme
-	
-	
+		root.theme = dark_theme if dark_mode else light_theme
