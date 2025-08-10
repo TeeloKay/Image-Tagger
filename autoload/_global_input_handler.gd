e1bd01f9a045ac8239d7d8673e895d2cf131713a
TeeloKay <143880606+TeeloKay@users.noreply.github.com>
2025-08-10 16:30:17 +0200

    added missing support tracking to search engine

--- autoload/_global_input_handler.gd
+++ autoload/_global_input_handler.gd
@@ -1,8 +1,10 @@
 extends Node
 
 signal apply_changes
+signal update
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("save_project"):
 		apply_changes.emit()
-		ProjectContext.save_current_project()
+	if event.is_action_pressed("update"):
+		update.emit()
